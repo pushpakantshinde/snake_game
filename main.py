b28a9a8ad9ea263823815cf2179b50514b06266a
Pushpakant Shindeclc <pushpakantshinde@gmail.com>
2023-10-09 18:26:48 +0530

    food module created, main updated

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
 from turtle import Screen
+import time
+from snake import Snake
+from food import Food
 
 my_screen = Screen()
 my_screen.setup(width=600, height=600)
 my_screen.bgcolor("black")
 my_screen.title("My Snake Game")
 my_screen.tracer(0)
+
+
+is_game_on = True
+snake = Snake()
+food_item = Food()
+
+my_screen.listen()
+my_screen.onkey(snake.up, "Up")
+my_screen.onkey(snake.down, "Down")
+my_screen.onkey(snake.left, "Left")
+my_screen.onkey(snake.right, "Right")
+
+
+while is_game_on:
+    my_screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food_item) < 20:
+        food_item.refresh()
+        snake.extend()
+
+    # Detect collision with wall
+    if (snake.head.xcor() > 280) or (snake.head.xcor() < -280) or (snake.head.ycor() > 280) or (snake.head.ycor() < -280):
+        snake.clear_snake()             # Clear the snake
+        snake.snake_reset()
+
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:  # distance 10 is decided based on trial & error
+            snake.clear_snake()           # Clear the snake
+            snake.snake_reset()
+
+
+my_screen.exitonclick()
